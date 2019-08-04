63073654eb172ac13f4f98f3195637f408911c67
Jakob Boisen <jakob@fumi.dk>
2019-08-04 12:42:04 +0200

    json
    added json

--- PassCalc/Program.cs
+++ PassCalc/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json;
+using PassCalc.Models;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -22,19 +25,65 @@ namespace PassCalc
         {
             Program p = new Program();
             p.run();
-           
+
         }
-        
+
         public void run()
         {
             processInfo = LeagueUtils.GetLeagueStatus();
 
             var summoner = LCU("/lol-summoner/v1/current-summoner");
             var summonermodel = JsonConvert.DeserializeObject<SummonerModel>(summoner);
+
             var catalog = LCU("/lol-loot/v1/player-loot-map");
+            var catalogmodel = JsonConvert.DeserializeObject<List<PlayerLootMapModel>>(catalog);
+
             var missions = LCU("/lol-missions/v1/missions");
+            var missionsmodel = JsonConvert.DeserializeObject<List<MissionModel>>(missions);
+
             var matchlist = LCU($"/lol-acs/v2/matchlists?accountId={summonermodel.AccountId}&begIndex=0&endIndex=100");
-            Console.WriteLine(matchlist);
+            var matchlistmodel = JsonConvert.DeserializeObject<List<MatchHistoryModel>>(matchlist);
+
+            var projectTokens = catalogmodel.Find(l => l.LootId == "MATERIAL_323");
+
+            long pointsFromMissions = 0;
+            var missionCount = 0;
+            var proejctMissions = missionsmodel.FindAll(m => m.SeriesName == "PROJECT2019_series");
+            var firstWinMissionget = proejctMissions.FindAll(m => m.MissionType == "REPEATING");
+            var pendingMissions = proejctMissions.FindAll(m => m.Status == "PENDING");
+            var firstWinMission = firstWinMissionget.Count() != 0 ? firstWinMissionget[0] : null;
+
+            foreach (var mission in pendingMissions)
+            {
+                var rewards = mission.Rewards.FindAll(r => r.ItemId == "lol_project_nonpremium_token");
+
+                foreach (var reward in rewards)
+                {
+                    missionCount++;
+                    pointsFromMissions += reward.Quantity;
+                }
+            }
+
+            var curTime = new DateTime();
+            var nextTime = new DateTime(2019, 8, 1, 1, 0, 0, 0);
+
+            var daysUntilEnd = (new DateTime(2019, 9, 2, 0, 0, 0) - nextTime).TotalDays;
+            var firstWinGains = daysUntilEnd * 18;
+            var requiredAmount = requirement - projectTokens.Count;
+            var playingEveryDayAmount = requiredAmount - firstWinGains;
+            var missionsRequiredAmount = playingEveryDayAmount - pointsFromMissions;
+            var avgAmountPerDay = missionsRequiredAmount / daysUntilEnd;
+            var aramWins = avgAmountPerDay / aramWinPoints;
+            var aramLosses = avgAmountPerDay / aramLosePoints;
+            var normalWins = avgAmountPerDay / normalWinPoints;
+            var normalLosses = avgAmountPerDay / normalLosePoints;
+            var tftWins = avgAmountPerDay / tftWinPoints;
+            var tftLosses = avgAmountPerDay / tftLosePoints;
+
+            var progressPerc = Math.Round((Convert.ToDouble(projectTokens.Count / requirement)) * 100);
+
+            Console.WriteLine($"You have {daysUntilEnd} days until the end of the event.");
+            Console.WriteLine($"You can still get {firstWinGains} tokens off of first win of the days.");
         }
         public string LCU(string url)
         {
@@ -56,7 +105,6 @@ namespace PassCalc
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64);
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var response = client.GetStringAsync(page).GetAwaiter().GetResult();
-                //response = response.Replace('"', ' ').Trim();
                 return response;
             }
         }
